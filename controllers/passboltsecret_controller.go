f78a445b6cc7805c8536e10f009dc212d93c747f
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2022-12-15 22:28:39 +0100

    feat: implemented passbolt secret reconciliation loop

--- controllers/passboltsecret_controller.go
+++ controllers/passboltsecret_controller.go
@@ -18,13 +18,18 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	corev1 "k8s.io/api/core/v1"
+
 	passboltv1alpha1 "github.com/urbanmedia/passbolt-operator/api/v1alpha1"
+	"github.com/urbanmedia/passbolt-operator/pkg/passbolt"
 )
 
 // PassboltSecretReconciler reconciles a PassboltSecret object
@@ -36,6 +41,7 @@ type PassboltSecretReconciler struct {
 //+kubebuilder:rbac:groups=passbolt.tagesspiegel.de,resources=passboltsecrets,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=passbolt.tagesspiegel.de,resources=passboltsecrets/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=passbolt.tagesspiegel.de,resources=passboltsecrets/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;create;update;delete;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -47,9 +53,64 @@ type PassboltSecretReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
 func (r *PassboltSecretReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	logr := log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	secret := &passboltv1alpha1.PassboltSecret{}
+	err := r.Client.Get(ctx, req.NamespacedName, secret)
+	if err != nil {
+		// If the resource no longer exists, in which case we stop processing.
+		logr.Error(err, "unable to fetch PassboltSecret")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
+
+	// FIXME: pass real values to the passbolt client
+	// create passbolt client
+	clnt, err := passbolt.NewClient(ctx, "", "", "")
+	if err != nil {
+		logr.Error(err, "unable to create passbolt client")
+		return ctrl.Result{}, err
+	}
+	defer clnt.Close(ctx)
+
+	// retrieve secrets from passbolt and store them in Kubernetes secrets
+	k8sSecret := &corev1.Secret{
+		ObjectMeta: ctrl.ObjectMeta{
+			Name:        secret.Name,
+			Namespace:   secret.Namespace,
+			Labels:      secret.Labels,
+			Annotations: secret.Annotations,
+		},
+	}
+	for _, secret := range secret.Spec.Secrets {
+		secretData, err := clnt.GetSecret(ctx, secret.Name)
+		if err != nil {
+			logr.Error(err, "unable to retrieve secret from passbolt")
+			return ctrl.Result{}, err
+		}
+		k8sSecret.StringData[secret.KubernetesSecretKey] = secretData
+	}
+
+	// check if the secret already exists
+	err = r.Client.Get(ctx, req.NamespacedName, &corev1.Secret{})
+	if err != nil && errors.IsNotFound(err) {
+		// fail if the error is not a not found error
+		return ctrl.Result{}, fmt.Errorf("unable to retrieve secret from kubernetes: %w", err)
+	}
+	// check if the secret already exists
+	if errors.IsNotFound(err) {
+		err = r.Client.Create(ctx, k8sSecret)
+		if err != nil {
+			logr.Error(err, "unable to create kubernetes secret")
+			return ctrl.Result{}, err
+		}
+		return ctrl.Result{}, nil
+	}
+	// secret already exists, update it
+	err = r.Client.Update(ctx, k8sSecret)
+	if err != nil {
+		logr.Error(err, "unable to update kubernetes secret")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
