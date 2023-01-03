9d3a9c675e79775c9b930d5d02d83c92e92ae40c
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-01-04 00:30:02 +0100

    test: added e2e passbolt controller tests

---
+++ controllers/passboltsecret_controller_test.go
@@ -0,0 +1,124 @@
+package controllers
+
+import (
+	"context"
+	"time"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	passboltv1alpha1 "github.com/urbanmedia/passbolt-operator/api/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+var _ = Describe("Run Controller", func() {
+
+	const (
+		timeout  = time.Second * 30
+		interval = time.Second * 1
+
+		name      = "example-passboltsecret"
+		namespace = "default"
+	)
+
+	BeforeEach(func() {
+		// Add any setup steps that needs to be executed before each test
+	})
+
+	AfterEach(func() {
+		// Add any teardown steps that needs to be executed after each test
+	})
+
+	// Add Tests for OpenAPI validation (or additonal CRD features) specified in
+	// your API definition.
+	// Avoid adding tests for vanilla CRUD operations because they would
+	// test Kubernetes API server, which isn't the goal here.
+	Context("Run directly without existing job", func() {
+		It("Should create successfully", func() {
+			Expect(1).To(Equal(1))
+		})
+	})
+
+	Context("Run existing job", func() {
+		It("Should create successfully", func() {
+			By("Create job for run")
+
+			passboltSecretSpec := passboltv1alpha1.PassboltSecretSpec{
+				LeaveOnDelete: false,
+				Secrets: []passboltv1alpha1.SecretSpec{
+					// TODO(@leonsteinhaeuser): add more secrets
+					{
+						KubernetesSecretKey: "password",
+						PassboltSecret: passboltv1alpha1.PassboltSpec{
+							Name:  "APP_EXAMPLE",
+							Field: passboltv1alpha1.FieldNamePassword,
+						},
+					},
+					{
+						KubernetesSecretKey: "url",
+						PassboltSecret: passboltv1alpha1.PassboltSpec{
+							Name:  "APP_EXAMPLE",
+							Field: passboltv1alpha1.FieldNameUri,
+						},
+					},
+					{
+						KubernetesSecretKey: "username",
+						PassboltSecret: passboltv1alpha1.PassboltSpec{
+							Name:  "APP_EXAMPLE",
+							Field: passboltv1alpha1.FieldNameUsername,
+						},
+					},
+				},
+			}
+
+			passboltSecret := &passboltv1alpha1.PassboltSecret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      name,
+					Namespace: namespace,
+				},
+				Spec: passboltSecretSpec,
+			}
+
+			By("By checking the PassboltSecret has been created")
+			// test if the passbolt secret is created
+			ctx := context.Background()
+			Expect(k8sClient.Create(ctx, passboltSecret)).Should(Succeed())
+			time.Sleep(time.Second * 60)
+			defer func() {
+				Expect(k8sClient.Delete(ctx, passboltSecret)).Should(Succeed())
+				time.Sleep(time.Second * 10)
+			}()
+
+			By("By checking the PassboltSecret can be retrieved")
+			passboltSecretKey := types.NamespacedName{Name: name, Namespace: namespace}
+			passboltSecretObj := &passboltv1alpha1.PassboltSecret{}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, passboltSecretKey, passboltSecretObj)
+			}, timeout, interval).Should(Succeed())
+
+			// By("By checking the PassboltSecret has been synced")
+			// Expect(passboltSecretObj.Status.SyncStatus).Should(Equal(passboltv1alpha1.SyncStatusSuccess))
+
+			By("By checking the PassboltSecret has been synced to Kubernetes Secret")
+			kubernetesSecretKey := passboltSecretKey
+			kubernetesSecretObj := &corev1.Secret{}
+			Eventually(func() error {
+				return k8sClient.Get(ctx, kubernetesSecretKey, kubernetesSecretObj)
+			}, timeout, interval).Should(Succeed())
+
+			By("By checking the Kubernetes Secret has three keys")
+			Expect(kubernetesSecretObj.Data).Should(HaveLen(3))
+
+			By("By checking the Kubernetes Secret has the username key")
+			Expect(kubernetesSecretObj.Data).Should(HaveKey("username"))
+
+			By("By checking the Kubernetes Secret has the url key")
+			Expect(kubernetesSecretObj.Data).Should(HaveKey("url"))
+
+			By("By checking the Kubernetes Secret has the password key")
+			Expect(kubernetesSecretObj.Data).Should(HaveKey("password"))
+		})
+	})
+})
