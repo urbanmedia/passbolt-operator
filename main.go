b5a27a9822fd73ba9ee24625f9c3ce8a964832b5
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2023-02-23 23:42:23 +0100

    enhancement: added PassboltSecret webhook service

--- main.go
+++ main.go
@@ -145,6 +145,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "PassboltSecret")
 		os.Exit(1)
 	}
+	if err = (&passboltv1alpha1.PassboltSecret{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "PassboltSecret")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
