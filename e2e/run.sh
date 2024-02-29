88d328d70b9cc6f8f7a7e7259568b4c88df3e737
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2024-02-29 12:08:12 +0100

    chore: added example passbolt secret for v1

--- e2e/run.sh
+++ e2e/run.sh
@@ -50,3 +50,25 @@ compareLength "6" ${payload_length}
 echo -e "${color_blue}Checking if Kubernetes secret ${color_yellow}${secret_name}${color_blue} exists and has the right .data length${NC}"
 payload_length=$(getKubernetesSecret ${secret_name} | jq -r ".data | length")
 compareLength "6" ${payload_length}
+
+############################
+########## Test 3 ##########
+############################
+api_version="v1"
+echo -e "${color_magenta}3: Testing API version: ${api_version}${color_reset}"
+secret_name="${api_version}-simple"
+createPassboltSecretV1alpha3 ${secret_name}
+
+api_version="v1alpha2"
+echo -e "${color_blue}Checking if Kubernetes secret ${color_yellow}${secret_name}${color_blue} in version ${color_yellow}${api_version}${color_blue} exists${NC}"
+payload_length=$(getPassboltSecret ${secret_name} ${api_version} | jq -r ".spec.secrets | length")
+compareLength "6" ${payload_length}
+
+api_version="v1"
+echo -e "${color_blue}Checking if Kubernetes secret ${color_yellow}${secret_name}${color_blue} in version ${color_yellow}${api_version}${color_blue} exists${NC}"
+payload_length=$(getPassboltSecret ${secret_name} ${api_version} | jq -r "(.spec.passboltSecrets | length) + (.spec.plainTextFields | length)")
+compareLength "6" ${payload_length}
+
+echo -e "${color_blue}Checking if Kubernetes secret ${color_yellow}${secret_name}${color_blue} exists and has the right .data length${NC}"
+payload_length=$(getKubernetesSecret ${secret_name} | jq -r ".data | length")
+compareLength "6" ${payload_length}
