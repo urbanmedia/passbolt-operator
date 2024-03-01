99ff884d4d2fa76d2a5537ea5997f1a842f7f4b4
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2024-03-01 12:21:07 +0100

    fix: added missing createPassboltSecretV1 function

--- e2e/run.sh
+++ e2e/run.sh
@@ -67,7 +67,7 @@ compareLength "6" ${payload_length}
 api_version="v1"
 echo -e "${color_magenta}3: Testing API version: ${api_version}${color_reset}"
 secret_name="${api_version}-simple"
-createPassboltSecretV1alpha3 ${secret_name}
+createPassboltSecretV1 ${secret_name}
 
 api_version="v1alpha2"
 echo -e "${color_blue}Checking if Kubernetes secret ${color_yellow}${secret_name}${color_blue} in version ${color_yellow}${api_version}${color_blue} exists${NC}"
