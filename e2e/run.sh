d8d334d6e45e07ca181a9e869c5f2ce7bc49e1a2
leonsteinhaeuser <leon.steinhaeuser@tagesspiegel.de>
2024-06-26 14:06:23 +0200

    feat: added backoff failure test

--- e2e/run.sh
+++ e2e/run.sh
@@ -87,3 +87,24 @@ compareLength "6" ${payload_length}
 echo -e "${color_blue}Checking if Kubernetes secret ${color_yellow}${secret_name}${color_blue} exists and has the right .data length${NC}"
 payload_length=$(getKubernetesSecret ${secret_name} | jq -r ".data | length")
 compareLength "6" ${payload_length}
+
+############################
+########## Test 4 ##########
+############################
+api_version="v1"
+echo -e "${color_magenta}4: Testing API version: ${api_version}${color_reset}"
+secret_name="${api_version}-backoff-check"
+createPassboltSecretV1WithSecretNotFound ${secret_name}
+
+echo -e $(getPassboltSecret ${secret_name} ${api_version})
+
+sync_status=$(getPassboltSecret ${secret_name} ${api_version} | jq -r ".status.lastSync")
+
+# check if status is not Success
+if [ "${sync_status}" != "Success" ]; then
+    echo -e "${color_red}Expected status to be not ${color_yellow}Success${color_red} but got ${color_yellow}${sync_status}${color_reset}"
+    exit 1
+fi
+
+payload_length=$(getPassboltSecret ${secret_name} ${api_version} | jq -r "(.spec.passboltSecrets | length) + (.spec.plainTextFields | length)")
+compareLength "0" ${payload_length}
